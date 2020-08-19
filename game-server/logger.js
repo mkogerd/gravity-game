30f410ebd0fa6d58d91c68c66512c52a285fd511
Koger Darden <mkogerd@gmail.com>
2020-08-19 00:17:15 -0500

    Remove routed logging in favor of docker logs

--- game-server/logger.js
+++ game-server/logger.js
@@ -1,5 +1,5 @@
 const { createLogger, format, transports } = require('winston');
-const { combine, timestamp, label, printf, colorize } = format;
+const { combine, timestamp, printf, colorize } = format;
 
 const consoleFormat = combine(
     timestamp(),
@@ -9,20 +9,22 @@ const consoleFormat = combine(
     ),
 );
 
-const fileFormat = combine(
-    timestamp(),
-    format.json(),
-);
+// Disabling the structured-log format fow now in favor of stdout log stream + docker logs
+// Keeping this format as a comment in case we want to change the stdout logs to json format for consumption and analysis
+// const fileFormat = combine(
+//     timestamp(),
+//     format.json(),
+// );
 
 const logger = createLogger({
     level: 'info',
     defaultMeta: { service: 'game-server' },
     transports: [
-        new transports.File({
-            filename: './logs/combined.log',
-            format: fileFormat,
-            handleExceptions: true,
-        }),
+        // new transports.File({
+        //     filename: './logs/combined.log',
+        //     format: fileFormat,
+        //     handleExceptions: true,
+        // }),
         new transports.Console({
             format: consoleFormat,
         }),
